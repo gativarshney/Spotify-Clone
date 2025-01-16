d7b295a311606301b06665465316f2c41b53cef7
gativarshney <gativarshney2006@gmail.com>
2025-01-16 21:26:10 +0530

    Update Library and PlayBar

--- script.js
+++ script.js
@@ -22,26 +22,22 @@ async function main(){
     let songs = await getSongs();
     console.log(songs);
 
+    // Show all the songs in the Library
     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
     for (const song of songs) {
-        songUL.innerHTML = songUL.innerHTML + `<li> ${song.replaceAll("_", " ")} </li>`;
+        songUL.innerHTML = songUL.innerHTML + `<li>
+                <img class="invert" src="images/music.svg" alt="">
+                <div class="info">
+                    <div>${song.replaceAll("_", " ")}</div>
+                    <div>Gati</div>
+                </div>
+                <div class="playnow">
+                    <span>Play Now</span>
+                    <img class="invert" src="images/play.svg" alt="">
+                </div> 
+            </li>`;
     }
 
-    // Play the first song
-    let audio = new Audio(songs[0]);
-    try{
-        // audio.play();
-        console.log("Song Played!");
-    }
-    catch(error){
-        console.log("Error: ", error);
-    }
-    
-    audio.addEventListener("loadeddata", ()=>{
-        let duration = audio.duration;
-        console.log("Duration: ", duration);
-        // The duration variable will hold the duration (in seconds) of the audio file.
-    })
     
 }
 main();
